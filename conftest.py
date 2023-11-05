353aafcb319873c5bc366d6ca01e986d2f268691
MariiaDo <124151591+MariiaDo@users.noreply.github.com>
2023-11-05 17:35:43 +0100

    "data parsing lesson done"

--- conftest.py
+++ conftest.py
@@ -1,16 +1,28 @@
+import json
+
 import pytest
 import random
+
+from constants import ROOT_PATH
 from utilities.config_reader import AppConfig
 from utilities.driver_factory import DriverFactory
+from utilities.json_to_class import DictToClass
 
 
 @pytest.fixture
-def create_driver():
-    driver = DriverFactory(AppConfig.browser_id).get_driver()
+def create_driver(env):
+    driver = DriverFactory(env.browser_id).get_driver()
     driver.maximize_window()
-    driver.get(AppConfig.url)
+    driver.get(env.url)
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def env():
+    with open(f"{ROOT_PATH}/configs/conf.json") as f:
+        conf_dict = json.loads(f.read())
+        return DictToClass(**conf_dict)
 
 
 @pytest.fixture
