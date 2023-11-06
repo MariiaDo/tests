998b6dc3731ee6a7e0d65523ed4cc2fd26f17273
MariiaDo <124151591+MariiaDo@users.noreply.github.com>
2023-11-06 21:36:11 +0100

    "api tests done"

---
+++ api_collections/schemas/json_schemas.py
@@ -0,0 +1,394 @@
+class NotesSchema:
+    all_notes = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            },
+            "data": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "id": {
+                            "type": "string"
+                        },
+                        "title": {
+                            "type": "string"
+                        },
+                        "description": {
+                            "type": "string"
+                        },
+                        "category": {
+                            "type": "string"
+                        },
+                        "completed": {
+                            "type": "boolean"
+                        },
+                        "created_at": {
+                            "type": "string"
+                        },
+                        "updated_at": {
+                            "type": "string"
+                        },
+                        "user_id": {
+                            "type": "string"
+                        }
+                    },
+                    "required": [
+                        "id",
+                        "title",
+                        "description",
+                        "category",
+                        "completed",
+                        "created_at",
+                        "updated_at",
+                        "user_id"
+                    ]
+                }
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message",
+            "data"
+        ]
+    }
+    get_note_by_id = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            },
+            "data": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string"
+                    },
+                    "title": {
+                        "type": "string"
+                    },
+                    "description": {
+                        "type": "string"
+                    },
+                    "category": {
+                        "type": "string"
+                    },
+                    "completed": {
+                        "type": "boolean"
+                    },
+                    "created_at": {
+                        "type": "string"
+                    },
+                    "updated_at": {
+                        "type": "string"
+                    },
+                    "user_id": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "title",
+                    "description",
+                    "category",
+                    "completed",
+                    "created_at",
+                    "updated_at",
+                    "user_id"
+                ]
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message",
+            "data"
+        ]
+    }
+    post_new_note = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            },
+            "data": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string"
+                    },
+                    "title": {
+                        "type": "string"
+                    },
+                    "description": {
+                        "type": "string"
+                    },
+                    "category": {
+                        "type": "string"
+                    },
+                    "completed": {
+                        "type": "boolean"
+                    },
+                    "created_at": {
+                        "type": "string"
+                    },
+                    "updated_at": {
+                        "type": "string"
+                    },
+                    "user_id": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "title",
+                    "description",
+                    "category",
+                    "completed",
+                    "created_at",
+                    "updated_at",
+                    "user_id"
+                ]
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message",
+            "data"
+        ]
+    }
+    put_notes_by_id = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            },
+            "data": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string"
+                    },
+                    "title": {
+                        "type": "string"
+                    },
+                    "description": {
+                        "type": "string"
+                    },
+                    "category": {
+                        "type": "string"
+                    },
+                    "completed": {
+                        "type": "boolean"
+                    },
+                    "created_at": {
+                        "type": "string"
+                    },
+                    "updated_at": {
+                        "type": "string"
+                    },
+                    "user_id": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "title",
+                    "description",
+                    "category",
+                    "completed",
+                    "created_at",
+                    "updated_at",
+                    "user_id"
+                ]
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message",
+            "data"
+        ]
+    }
+    patch_completed_by_id = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            },
+            "data": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string"
+                    },
+                    "title": {
+                        "type": "string"
+                    },
+                    "description": {
+                        "type": "string"
+                    },
+                    "category": {
+                        "type": "string"
+                    },
+                    "completed": {
+                        "type": "boolean"
+                    },
+                    "created_at": {
+                        "type": "string"
+                    },
+                    "updated_at": {
+                        "type": "string"
+                    },
+                    "user_id": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "title",
+                    "description",
+                    "category",
+                    "completed",
+                    "created_at",
+                    "updated_at",
+                    "user_id"
+                ]
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message",
+            "data"
+        ]
+    }
+    delete_note_by_id = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message"
+        ]
+    }
+    get_note_by_id_negative = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message"
+        ]
+    }
+    post_new_note_negative = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message"
+        ]
+    }
+    put_note_by_id_negative = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message"
+        ]
+    }
+    delete_note_by_id_negative = {
+        "type": "object",
+        "properties": {
+            "success": {
+                "type": "boolean"
+            },
+            "status": {
+                "type": "number"
+            },
+            "message": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "success",
+            "status",
+            "message"
+        ]
+    }
